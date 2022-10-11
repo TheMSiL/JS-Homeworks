eb23eaa2f373051567dda5864274262904a534ea
TheMSiL <silka.maksim15@gmail.com>
2022-10-11 20:06:02 +0300

    homework-5 done

--- Homework-5/script.js
+++ Homework-5/script.js
@@ -44,71 +44,29 @@ const authorize = () => {
     } while (!isAuthSuccess); 
 };
  
-// authorize ();
-
-                                                        // Вывести простые числа в заданном промежутке
-
-                                                        // Натуральное число, большее 1, называется простым, если оно ни на что не делится, кроме себя и 1.
-
-                                                        // Реализуте функцию, которая будет принимать 2 аргумента (от какого до какого числа искать простые числа)
-                                                        // Поочередно выведите в консоль простые числа из данного промежутка.
-                                                        // Например:
-
-                                                        // для промежутка 1 и 10 результат будет 2,3,5,7
-                                                        // для 20 и 100 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+authorize ();
 
                                             // Второе задание
-// const getNumber = () => {
-//     // let a = Number (prompt('First number :'));
-//     // let b = Number (prompt('Second number :'));
-    
-        
-//    }
-// }
 
-// getNumber ();
+let i = Number (prompt('Первое число:'));  
+let f = Number (prompt('Второе число:'));
 
-let n = 10;
+const showNaturalNumbers = (i,f) => {
+    if (i === 1) {
+        i = 2;
+    };
 
-outFor:
-for (let i = 2; i <= n; i++) {
-
-    for (let j = 2; j < 1; j++) {
-        if (i % j == 0) continue outFor;
+    if(f > 1) {
+    nextVar:
+    for (;i <= f; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0){
+            continue nextVar;
+            } 
+        }
+        console.log(i); 
+        }
     }
-
-    console.log(i);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const drawPyramid = (height) => {
-//     for (let i = 0; i < height; i++){
-//         let spacesBefore = '';
-//         let stars = '';
-
-//         for (let j = 0; j < 2 * i + 1; j++){
-//             stars += '*';
-//         }
-
-//         for (let k = 0; k < height - i -1; k++) {
-//             spacesBefore += ' ';
-//         }
-
-//         console.log(spacesBefore + stars)
-//     } 
-// };
-
-// const desireHeight = Number(prompt('Какая высота пирамиды?'));
-
-// drawPyramid (desireHeight);
+showNaturalNumbers(i,f);
