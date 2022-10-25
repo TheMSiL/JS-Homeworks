406f1cb47b4175344c9fcf4d4150bc5d83a191d7
TheMSiL <silka.maksim15@gmail.com>
2022-10-25 17:13:51 +0300

    Homework-8 done

---
+++ Homework-8/script.js
@@ -0,0 +1,69 @@
+                                                      // !Первое задание
+                                                      
+                                                      
+let arr = [];
+
+function getNumber (arr) {
+
+let num = 0;
+for (let truth of arr) {
+   if (truth = truth) { // (truth === true)
+      num++
+   }
+}
+   console.log(num);
+}
+
+getNumber([true, false, false, true, false]);
+getNumber([false, false, false, false]);
+getNumber([]);
+
+                                                      // !Второе задание
+
+let array = [];
+let object = {};
+function getOccurrencesCount (array) {
+   for (let elem of array) {
+      if (object[elem] === undefined) {
+         object[elem] = 1;
+      } else {
+         object[elem]++;
+      }
+   }
+   return object
+}
+console.log(getOccurrencesCount(["a", "v", "a", "b", "b"]));
+console.log(getOccurrencesCount(["apples","oranges",
+"pears","pears","apples","oranges","oranges","pears",]));
+
+                                                      // !Третье задание
+
+const findExcess = (arrAy) => {
+
+let superfluous = 0;
+let arr1 = [];
+let arr2 = [];
+   
+for (let num of arrAy) {
+
+   if (num % 2 !== 0) {
+      arr1.push(num);
+   } else {
+      arr2.push(num);
+   }
+
+   if ((arr1.length === 1) && (arr1.length < arr2.length)) {
+      superfluous = arr1[0];
+   } else {
+      superfluous = arr2[0];
+   }
+} 
+     
+return superfluous
+}
+      
+console.log(findExcess([0, 1, 2]));// -> 1
+console.log(findExcess([1, 2, 3]));// -> 2
+console.log(findExcess([2, 6, 8, 10, 3])); // -> 3
+console.log(findExcess([0, 0, 3, 0, 0])); // -> 3
+console.log(findExcess([1, 1, 0, 1, 1])); // -> 0
