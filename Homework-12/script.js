2cc336a636cf238b9c5fd71d3fb506f91227f632
TheMSiL <silka.maksim15@gmail.com>
2022-11-26 06:32:22 +0200

    fix homework 12

--- Homework-12/script.js
+++ Homework-12/script.js
@@ -1,42 +1,19 @@
-// const button = document.getElementById('button');
-// button.className = 'button';
-
-
-// const createTooltip = (elem, text) => {
-//   const coords = elem.getBoundingClientRect();
-
-//   const tooltip = document.createElement('div');
-//   tooltip.innerText = text;
-//   tooltip.className = 'text';
-//   tooltip.style = 'position: absolute; top: 80px; left: 80px';
-
-
-//   document.body.prepend(tooltip);
-// }  
-
-// createTooltip(button, 'No light - no life!');
-
-
 const button = document.getElementById('button');
-button.classList.add('button');
+button.className = 'button';
 
 
 const createTooltip = (elem, text) => {
-  
+  const coords = elem.getBoundingClientRect();
+
   const tooltip = document.createElement('div');
   tooltip.innerText = text;
   tooltip.className = 'text';
+  tooltip.style = 'position: absolute; top: 80px; left: 80px';
+
+
   document.body.prepend(tooltip);
-
-  const coords = elem.getBoundingClientRect();
-
-  const tooltipTop = coords.top - 10 - tooltip.clientHeight;
-  
-  const tooltipLeft = coords.left + coords.width / 2 - tooltip.clientWidth / 2;
-
-  tooltip.style.top = `${tooltipTop}px`;
-  tooltip.style.left = `${tooltipLeft}px`;
-
 }  
 
 createTooltip(button, 'No light - no life!');
+
+
